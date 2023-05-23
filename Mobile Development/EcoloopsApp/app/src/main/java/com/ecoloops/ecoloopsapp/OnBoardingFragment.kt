77aec44f9af32ec9c78135090caf07246834c30e
Cahya Diantoni <58876253+cahyadiantoni@users.noreply.github.com>
2023-05-23 20:37:23 +0700

    on boarding appears once

---
+++ Mobile Development/EcoloopsApp/app/src/main/java/com/ecoloops/ecoloopsapp/OnBoardingFragment.kt
@@ -0,0 +1,49 @@
+package com.ecoloops.ecoloopsapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
+import com.ecoloops.ecoloopsapp.screens.FirstScreen
+import com.ecoloops.ecoloopsapp.screens.SecondScreen
+import com.ecoloops.ecoloopsapp.screens.ThirdScreen
+import com.tbuonomo.viewpagerdotsindicator.DotsIndicator
+
+
+class OnBoardingFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_on_boarding, container, false)
+
+
+        val fragmentList = arrayListOf<Fragment>(
+            FirstScreen(),
+            SecondScreen(),
+            ThirdScreen()
+        )
+
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+        val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
+
+        viewPager.adapter = adapter
+        val indicator = view.findViewById<DotsIndicator>(R.id.dots_indicator)
+
+        indicator.attachTo(viewPager)
+
+        return view
+    }
+
+
+}
