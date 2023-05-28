4d80affa9337aaf9f68c4c7c763c17cd1076f25a
Azka Zufar Hanif <78843111+azkazufarh@users.noreply.github.com>
2023-05-28 09:13:40 +0700

    Revert "Azka cc"

--- Cloud Computing/server.js
+++ Cloud Computing/server.js
@@ -1,37 +1,16 @@
 const express = require("express");
-const db = require("./config/db-config");
-const authRouter = require("./routes/routes");
 const cors = require("cors");
-const session = require("express-session");
-const bodyParser = require("body-parser");
 
-const PORT = process.env.PORT || 8000;
 const app = express();
-app.use(
-  session({
-    secret: "your-secret-key",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
+
+const port = 8080;
+
 app.use(cors("*"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log("database connected");
+app.get('/test', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.get("/test", (req, res) => {
-  res.status(200).send({
-    status: "success",
-    message: "API is running",
-  });
-});
-
-app.use("/", authRouter);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port: http://localhost:${PORT}/`);
+app.listen(port, () => {
+    console.log(`Server berjalan di http://localhost:${port}`);
 });
