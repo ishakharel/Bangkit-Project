ecb3ac77719606bcc8b8e28cc05b9a11fd8e0ff0
Agil Fikriawan <agilfikriawan2803@gmail.com>
2023-05-31 11:28:45 +0800

    Fixed error issues

--- Cloud Computing/routes/routes.js
+++ Cloud Computing/routes/routes.js
@@ -9,6 +9,7 @@ const userController = require("../controllers/user");
 //Authentication
 router.post("/auth/register", checkApiKey, authController.register);
 router.post("/auth/login", checkApiKey, authController.login);
+router.post("/auth/logout", checkApiKey, checkAuth, authController.logout);
 router.post("/auth/forgot-pass", checkApiKey, authController.forgotPassword);
 router.put("/auth/reset-pass", checkApiKey, authController.resetPassword);
 router.put("/auth/change-pass", checkApiKey, checkAuth, authController.changePassword);
@@ -22,13 +23,13 @@ router.get("/user/points/", checkApiKey, checkAuth, userController.checkPoints);
 router.get("/user/merch-redeemed/", checkApiKey, checkAuth, userController.getMerchRedeemedByUserId);
 router.put("/user/points/", checkApiKey, checkAuth, userController.exchangePoints);
 router.put("/user/username/", checkApiKey, checkAuth,userController.changeUsername);
-router.delete("/user/notif/", checkApiKey, checkAuth, userController.deleteNotificationById);
+router.delete("/user/notif/:id", checkApiKey, checkAuth, userController.deleteNotificationById);
 
 //Waste
-router.get("/waste/categories", checkApiKey, wasteController.categories);
+router.get("/waste/categories/", checkApiKey, wasteController.categories);
 router.get("/waste/histories/", checkApiKey, checkAuth, wasteController.histories);
-router.get("/waste/histories/:history_id", checkApiKey, checkAuth,wasteController.historyDetail);
-router.get("/waste/categories/:category_id", checkApiKey, checkAuth,wasteController.categoryById);
-router.post("/waste/upload", checkApiKey, checkAuth, wasteController.upload);
+router.get("/waste/histories/:history_id", checkApiKey, checkAuth, wasteController.historyDetail);
+router.get("/waste/categories/:category_id", checkApiKey, checkAuth, wasteController.categoryById);
+router.post("/waste/upload/", checkApiKey, checkAuth, wasteController.upload);
 
 module.exports = router;
