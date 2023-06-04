d04d4a1fe088cb6e0232e456a03612b30e324103
Agil Fikriawan <agilfikriawan2803@gmail.com>
2023-06-04 21:27:32 +0800

    Revised some code at waste, routes and server

--- Cloud Computing/controllers/waste.js
+++ Cloud Computing/controllers/waste.js
@@ -6,6 +6,7 @@ const sharp = require("sharp");
 const axios = require("axios");
 const fs = require("fs");
 const FormData = require("form-data");
+const { error } = require("console");
 require("dotenv").config();
 
 const categories = (req, res) => {
@@ -41,7 +42,7 @@ const histories = (req, res) => {
   const userId = req.user.id;
 
   db.query(
-    "SELECT a.*, b.name as waste_name, b.category FROM waste_history a JOIN waste_category b ON a.category_id = b.id WHERE user_id = ?",
+    "SELECT a.id, b.name as name, b.category as category, a.date as date, a.point as points, a.image FROM waste_history a JOIN waste_category b ON a.category_id = b.id WHERE a.user_id = ?",
     [userId],
     (error, result) => {
       if (error) {
@@ -59,7 +60,7 @@ const historyDetail = (req, res) => {
   const { id } = req.params;
 
   db.query(
-    "SELECT a.id, b.name as waste_name, b.category FROM waste_history a JOIN waste_category b ON a.category_id = b.id WHERE user_id = ? AND a.id = ?",
+    "SELECT b.name as name, b.category as category, a.date as date, a.point as points FROM waste_history a JOIN waste_category b ON a.category_id = b.id WHERE a.user_id = ? AND a.id = ?",
     [userId, id],
     (error, result) => {
       if (error) {
@@ -123,6 +124,9 @@ const upload = async (req, res) => {
     const blob = bucket.file("waste_history/" + id);
     const blobStream = blob.createWriteStream({
       resumable: false,
+      metadata: {
+        contentType: req.file.mimetype,
+      },
     });
 
     blobStream.on(true, (err) => {
@@ -135,27 +139,39 @@ const upload = async (req, res) => {
         `https://storage.googleapis.com/${bucket.name}/${blob.name}`
       );
 
-      const insertWaste = "INSERT INTO waste_history VALUES (?, ?, ?, ?, ?, ?)";
-      const valuesWaste = [id, userId, categoryId, publicUrl, 100, new Date()];
-      const updatePoints =
-        "UPDATE users SET total_points = total_points + 100 WHERE id = ?";
-
       try {
         // Make the file public
         await bucket.file(req.file.originalname).makePublic();
       } catch {
-        db.query(insertWaste, valuesWaste, (err, result1) => {
-          db.query(updatePoints, [userId], (err, result) => {
-            if (err) {
-              res.status(400).json({
-                error: true,
-                message: "Error connection!",
-              });
-            }
-
-            res.status(201).json({
-              status: "success",
-              message: "Successfully upload!",
+        db.query("SELECT * FROM waste_category WHERE id = ? ", [categoryId], (error, results) => {
+          const points = results[0].points
+          const insertWaste = "INSERT INTO waste_history VALUES (?, ?, ?, ?, ?, ?)";
+          const valuesWaste = [id, userId, categoryId, publicUrl, points, new Date()];
+          const updatePoints = "UPDATE users SET total_points = total_points + ? WHERE id = ?";
+          db.query(insertWaste, valuesWaste, (err, result1) => {
+            db.query(updatePoints, [points, userId], (err, result) => {
+              if (err) {
+                res.status(400).json({
+                  error: true,
+                  message: "Error connection!",
+                });
+              }
+  
+              db.query(
+                "SELECT a.id, b.name as name, b.category as category, b.description_recycle as description_recycle, a.date as date, a.point as points, a.image FROM waste_history a JOIN waste_category b ON a.category_id = b.id WHERE a.user_id = ? AND a.id = ? ",
+                [userId, id],
+                (error, results) => {
+                  if (error) {
+                    console.log(error);
+                    return res.status(500).send("Server Error!");
+                  }
+                  res.status(200).json({
+                    status: 'success',
+                    message: "Successfully upload!",
+                    data: results
+                  });
+                }
+              );
             });
           });
         });
--- Cloud Computing/routes/routes.js
+++ Cloud Computing/routes/routes.js
@@ -8,85 +8,30 @@ const userController = require("../controllers/user");
 const { multerConfig } = require("../config/storage-config");
 
 //Authentication
-router.post("/auth/register", checkApiKey, authController.register);
-router.post("/auth/login", checkApiKey, authController.login);
-router.post("/auth/logout", checkApiKey, checkAuth, authController.logout);
-router.post("/auth/forgot-pass", checkApiKey, authController.forgotPassword);
-router.put("/auth/reset-pass", checkApiKey, authController.resetPassword);
-router.put(
-  "/auth/change-pass",
-  checkApiKey,
-  checkAuth,
-  authController.changePassword
-);
+router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
+router.post("/auth/logout", checkAuth, authController.logout);
+router.post("/auth/forgot-pass", authController.forgotPassword);
+router.put("/auth/reset-pass", authController.resetPassword);
+router.put("/auth/change-pass", checkAuth, authController.changePassword);
 
 //User
-router.post(
-  "/user/notif",
-  checkApiKey,
-  checkAuth,
-  userController.addNotification
-);
-router.get(
-  "/user/notif/",
-  checkApiKey,
-  checkAuth,
-  userController.getNotificationByUserId
-);
-router.get("/user", checkApiKey, checkAuth, userController.getUserById);
-router.get("/user/merch/", checkApiKey, userController.getAllMerch);
-router.get("/user/points/", checkApiKey, checkAuth, userController.checkPoints);
-router.get(
-  "/user/merch-redeemed/",
-  checkApiKey,
-  checkAuth,
-  userController.getMerchRedeemedByUserId
-);
-router.put(
-  "/user/points/",
-  checkApiKey,
-  checkAuth,
-  userController.exchangePoints
-);
-router.put(
-  "/user/username/",
-  checkApiKey,
-  checkAuth,
-  userController.changeUsername
-);
-router.put(
-  "/user/profile/",
-  checkApiKey,
-  checkAuth,
-  multerConfig,
-  userController.changeImage
-);
-router.delete(
-  "/user/notif/:id",
-  checkApiKey,
-  checkAuth,
-  userController.deleteNotificationById
-);
+router.post("/user/notif", checkAuth, userController.addNotification);
+router.get("/user/notif/", checkAuth, userController.getNotificationByUserId);
+router.get("/user", checkAuth, userController.getUserById);
+router.get("/user/merch/", userController.getAllMerch);
+router.get("/user/points/", checkAuth, userController.checkPoints);
+router.get("/user/merch-redeemed/", checkAuth, userController.getMerchRedeemedByUserId);
+router.put("/user/points/", checkAuth, userController.exchangePoints);
+router.put("/user/username/",checkAuth, userController.changeUsername);
+router.put("/user/profile/", checkAuth, multerConfig, userController.changeImage);
+router.delete("/user/notif/:id", checkAuth, userController.deleteNotificationById);
 
 //Waste
-router.get("/waste/categories/", checkApiKey, wasteController.categories);
-router.get(
-  "/waste/histories/",
-  checkApiKey,
-  checkAuth,
-  wasteController.histories
-);
-router.get(
-  "/waste/histories/:id",
-  checkApiKey,
-  checkAuth,
-  wasteController.historyDetail
-);
-router.get(
-  "/waste/categories/:category_id",
-  checkApiKey,
-  wasteController.categoryById
-);
-router.post("/waste/upload/", checkApiKey, checkAuth, wasteController.upload);
+router.get("/waste/categories/", wasteController.categories);
+router.get("/waste/histories/", checkAuth, wasteController.histories);
+router.get("/waste/histories/:id", checkAuth, wasteController.historyDetail);
+router.get("/waste/categories/:category_id", wasteController.categoryById);
+router.post("/waste/upload/", checkAuth, wasteController.upload);
 
 module.exports = router;
