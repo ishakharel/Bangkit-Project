185c3cd34d98004f4a2ab383133168b1c1516833
Arina Sabilahaq <arina.sabilahaq@outlook.co.id>
2023-06-07 09:49:03 +0700

    Cardview Category

--- EcoloopsApp/app/src/main/java/com/ecoloops/ecoloopsapp/ui/page/HomeFragment.kt
+++ EcoloopsApp/app/src/main/java/com/ecoloops/ecoloopsapp/ui/page/HomeFragment.kt
@@ -5,25 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.ecoloops.ecoloopsapp.R
+import com.ecoloops.ecoloopsapp.ui.page.data.CategoryAdapter
+import com.ecoloops.ecoloopsapp.ui.page.data.CategoryItem
 
 class HomeFragment : Fragment() {
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-
-    }
+    private lateinit var recyclerView: RecyclerView
+    private val categoryList = ArrayList<CategoryItem>()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_home, container, false)
+
+        recyclerView = rootView.findViewById(R.id.parent_rv_category)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        addDataToList()
+        val adapter = CategoryAdapter(categoryList)
+        recyclerView.adapter = adapter
+
+        return rootView
     }
 
+    private fun addDataToList() {
 
+        categoryList.add(CategoryItem("Medis", R.drawable.medic))
+        categoryList.add(CategoryItem("Logam", R.drawable.metal))
+        categoryList.add(CategoryItem("Plastik", R.drawable.plastic))
+        categoryList.add(CategoryItem("Glass", R.drawable.glass))
+        categoryList.add(CategoryItem("Karton", R.drawable.cardboard))
+        categoryList.add(CategoryItem("Kertas", R.drawable.paper))
+        
+    }
 }
+
+
 
---
+++ EcoloopsApp/app/src/main/java/com/ecoloops/ecoloopsapp/ui/page/data/CategoryItem.kt
@@ -0,0 +1,3 @@
+package com.ecoloops.ecoloopsapp.ui.page.data
+
+data class CategoryItem(val title : String, val logo : Int)
