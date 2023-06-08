4ff1490ea7b1a555b80080ac8ab72a108e664e92
Arina Sabilahaq <arina.sabilahaq@outlook.co.id>
2023-06-08 18:54:43 +0700

    Detail Category Activity

---
+++ EcoloopsApp/app/src/main/java/com/ecoloops/ecoloopsapp/ui/page/category/DetailCategoryActivity.kt
@@ -0,0 +1,12 @@
+package com.ecoloops.ecoloopsapp.ui.page.category
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ecoloops.ecoloopsapp.R
+
+class DetailCategoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_category)
+    }
+}
