0ce88bb80cde5ff01412b0e5e472cbc97b805e99
Arina Sabilahaq <arina.sabilahaq@outlook.co.id>
2023-06-09 23:09:02 +0700

    notification and history

---
+++ EcoloopsApp/app/src/main/java/com/ecoloops/ecoloopsapp/ui/page/HistoryFragment.kt
@@ -0,0 +1,49 @@
+package com.ecoloops.ecoloopsapp.ui.page
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ecoloops.ecoloopsapp.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [HistoryFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class HistoryFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_history, container, false)
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment HistoryFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            HistoryFragment().apply {
+
+            }
+    }
+}
